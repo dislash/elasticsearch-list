8497748a85a5df0b3402dc2116da00ecad63cf7a
dislash <forjava92@gmail.com>
2019-11-04 22:36:12 +0900

    update main.py

--- main.py
+++ main.py
@@ -25,12 +25,10 @@ def index():
         items = []
         entry = {'id':tag1.key_as_string, 'title':tag1.doc_count}
         rows.append(entry)
-        for tag2 in tag1.1.buckets:
-            for tag3 in tag2.am.buckets:
-                for tag4 in tag3.by_time.buckets:
-                    item = {'category_count':tag2.doc_count, 'category':'1', 'am_pm':tag3.key, 'time':str(tag4.key)}
-                    items.append(item)
-                    entry[tag1.key_as_string]=items
+        for tag2 in tag1.1.am.by_time.buckets:
+            item = {'1amtime':str(tag4.key)}
+            items.append(item)
+            entry[tag1.key_as_string]=items
     print(rows)
     return render_template('list.html', books=rows)
 
